778ef4df7bd3163add6995ba955cb356873f7048
Non-Contradiction <lch34677@gmail.com>
2017-07-06 21:23:19 -0400

    Try to use Travis to deploy gh-pages automatically.

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf out
+mkdir out
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build (CHANGE THIS)
+jupyter nbconvert Lecture*.ipynb --to slides --output-dir ./out
+
+# deploy
+cd out
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
